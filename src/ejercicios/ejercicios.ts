bbaedc14821f8cda992e6fc391a09579f3a503e1
David Vaquero <pepesan@gmail.com>
2025-04-07 13:44:51 +0200

    corregidos potenciales fallos al compilar de manera estricta

--- src/ejercicios/ejercicios.ts
+++ src/ejercicios/ejercicios.ts
@@ -1,75 +1,79 @@
-export function ejercicios(element: HTMLParagraphElement):void{
+export function ejercicios(element: HTMLParagraphElement | null):void{
     console.log("ejercicios");
     // Crea una variable de cada tipo siguiente: null, boolean, number, string y presentalas por pantalla dentro de un párrafo
     var nulo: null = null;
     var booleano: boolean = false;
     var numero: number = 2;
     var cadena: string = "Hola";
-    element.innerHTML = "nulo: " + nulo + ", booleano: " + booleano + ", numero: " + numero + ", cadena: " + cadena;
-    // Crea un tres de variables de tipo number, suma las dos primeras y coloca el valor de la suma por pantalla
-    var i: number = 1;
-    var j: number = 2;
-    var k: number = i+j;
-    element.innerHTML = element.innerHTML + ", suma: " + k;
-    // Declara dos variables con var y let y una constante con const
-    var var1: number = 0;
-    let var2: number = 0;
-    const var3: number = 0;
+    if (element!== null){
+        element.innerHTML = "nulo: " + nulo + ", booleano: " + booleano + ", numero: " + numero + ", cadena: " + cadena;
+        // Crea un tres de variables de tipo number, suma las dos primeras y coloca el valor de la suma por pantalla
+        var i: number = 1;
+        var j: number = 2;
+        var k: number = i+j;
+        element.innerHTML = element.innerHTML + ", suma: " + k;
+        // Declara dos variables con var y let y una constante con const
+        var var1: number = 0;
+        console.log(var1);
+        let var2: number = 0;
+        console.log(var2);
+        const var3: number = 0;
+        console.log(var3);
 
-    /*
-        Crea un array de 10 números como variable
-        Recorre el array con: for, while y do while
-        Recorre el array con for of y for in
-        Imprime todas las iteraciones por pantalla
-     */
-    var arr = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
-    for (let i = 0; i < 10; i++) {
-        console.log(arr[i]);
-    }
-    var i = 0;
-    while (i<arr.length) {
-        console.log(arr[i]);
-        i++;
-    }
-    i = 0;
-    do {
-        console.log(arr[i]);
-        i++;
-    }while (i < arr.length);
-
-    for (let val of arr) {
-        console.log(val); // prints values: 10, 20, 30, 40
-    }
-
-    for (let index in arr) {
-        console.log(index); // prints indexes: 0, 1, 2, 3, 4
-
-        console.log(arr[index]); // prints elements: 10, 20, 30, 40, 50
-    }
-    // Guarda un dato en LocalStorage y recupéralo, comprueba con try catch que no tienes ningún error, y presenta el valor por pantalla
-    try {
-        // Almacenar un dato en el LocalStorage
-        localStorage.setItem("dato", "valor de prueba");
-
-        // Recuperar el dato del LocalStorage
-        const valorRecuperado = localStorage.getItem("dato");
-
-        // Verificar que se haya obtenido un valor
-        if (valorRecuperado !== null) {
-            // Mostrar el valor en la consola
-            console.log("Valor recuperado:", valorRecuperado);
-
-            // También se puede mostrar el valor en la pantalla, por ejemplo, en el body del documento
-            element.innerHTML = element.innerHTML + `, Valor recuperado: ${valorRecuperado}`;
-        } else {
-            console.log("No se encontró ningún dato en LocalStorage.");
+        /*
+            Crea un array de 10 números como variable
+            Recorre el array con: for, while y do while
+            Recorre el array con for of y for in
+            Imprime todas las iteraciones por pantalla
+         */
+        var arr = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
+        for (let i = 0; i < 10; i++) {
+            console.log(arr[i]);
         }
-    } catch (error) {
-        // Capturar y mostrar cualquier error que pueda ocurrir
-        console.error("Error al acceder a LocalStorage:", error);
+        var i = 0;
+        while (i<arr.length) {
+            console.log(arr[i]);
+            i++;
+        }
+        i = 0;
+        do {
+            console.log(arr[i]);
+            i++;
+        }while (i < arr.length);
+
+        for (let val of arr) {
+            console.log(val); // prints values: 10, 20, 30, 40
+        }
+
+        for (let index in arr) {
+            console.log(index); // prints indexes: 0, 1, 2, 3, 4
+
+            console.log(arr[index]); // prints elements: 10, 20, 30, 40, 50
+        }
+        // Guarda un dato en LocalStorage y recupéralo, comprueba con try catch que no tienes ningún error, y presenta el valor por pantalla
+        try {
+            // Almacenar un dato en el LocalStorage
+            localStorage.setItem("dato", "valor de prueba");
+
+            // Recuperar el dato del LocalStorage
+            const valorRecuperado = localStorage.getItem("dato");
+
+            // Verificar que se haya obtenido un valor
+            if (valorRecuperado !== null) {
+                // Mostrar el valor en la consola
+                console.log("Valor recuperado:", valorRecuperado);
+
+                // También se puede mostrar el valor en la pantalla, por ejemplo, en el body del documento
+                element.innerHTML = element.innerHTML + `, Valor recuperado: ${valorRecuperado}`;
+            } else {
+                console.log("No se encontró ningún dato en LocalStorage.");
+            }
+        } catch (error) {
+            // Capturar y mostrar cualquier error que pueda ocurrir
+            console.error("Error al acceder a LocalStorage:", error);
+        }
+
     }
-
-
     // 1. Función que imprime un valor por consola
     function printValue(value: any): void {
         console.log(value);
