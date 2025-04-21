f3fa2765aa2244d3ebb183eff3de28eedf22f878
David Vaquero <pepesan@gmail.com>
2025-04-21 11:53:03 +0200

    mejoras de los ejemplos y actualización de dependencias

--- src/modulos/tipos.ts
+++ src/modulos/tipos.ts
@@ -4,22 +4,27 @@ export function tipos(){
     // tipos de datos básicos
     console.log("Ejemplos de tipos");
     // cadenas
-    var cadena = "Hola"
+    var cadena: string = "Hola";
     console.log(cadena);
     // numeros
-    var numero = 2;
+    var numero: number = 2;
     console.log(numero);
     // incluso con float
-    var numero2 = 3.2;
+    var numero2: number = 3.2;
     console.log(numero2);
     // booleanos
-    var booleano = true;
+    var booleano: boolean = true;
     console.log(booleano);
 
     // Tipos definidos con tipo
 
-    var cadena: string = "Hola"
-    var numero: number = 2;
-    var booleano: boolean = true;
+    var cadena2: string = "Hola"
+    var numero2: number = 2;
+    var booleano2: boolean = true;
+
+    // variables definidas sin tipo
+    var cadena3 = "Hola"
+    var numero3 = 2;
+    var booleano3 = true;
 
 }
